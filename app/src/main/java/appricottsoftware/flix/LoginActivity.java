1c7eecec8f87aefc1275b5106505333f92d6cf57
Karen Kong <kkong006@ucr.edu>
2018-02-15 14:31:17 -0800

    Add login activity

---
+++ app/src/main/java/appricottsoftware/flix/LoginActivity.java
@@ -0,0 +1,24 @@
+package appricottsoftware.flix;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_login)
+    public void onClickLogin() {
+        Intent i = new Intent(this, MovieListActivity.class);
+        startActivity(i);
+    }
+}
